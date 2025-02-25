cca9748cf894f6e4f5c827f32c5f8ec0339ec78d
Kaio W. F. Costa <kaiowfc15@gmail.com>
2025-02-25 19:47:56 -0300

    att 1.1

--- hello/player.gd
+++ hello/player.gd
@@ -1,25 +1,34 @@
 extends CharacterBody2D
 
+# Variáveis de configuração
+var speed = 300.0
+var jump_speed = -500.0
+# Pega a gravidade das configurações do projeto
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+func _physics_process(delta):
+	# Adiciona a gravidade apenas se não estiver no chão
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Manipula o pulo
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		$SoundJump.play()
+		velocity.y = jump_speed
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	# Obtém a direção do input
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * speed
+
+	# Atualiza a animação
+	if direction > 0:
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.play()
+	elif direction < 0:
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.stop()
 
+	# Movimenta o personagem
 	move_and_slide()
